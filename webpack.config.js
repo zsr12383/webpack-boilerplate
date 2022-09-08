18895b822129ce2b4bdde93f4e5695a978153aac
wuhanvirus <62196278+wuhanvirus@users.noreply.github.com>
2022-09-08 23:54:13 +0900

    feat: 로더들 추가

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,42 @@ const config = {
     path: path.resolve(__dirname, "./dist"),
     filename: "build.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      // {
+      //   test: /\.(png|jpe?g|gif|svg|ico)$/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options: {
+      //         name: "images/[name].[ext]?[hash]",
+      //       }
+      //     },
+      //   ],
+      // },
+      // {
+      //   loader: "url-loader",
+      //   options: {
+      //     name: "images/[name].[ext]?[hash]",
+      //     limit: 10000
+      //   }
+      // },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css", // 원하는 filename
+    }),
+  ],
 };
 
 module.exports = config;
